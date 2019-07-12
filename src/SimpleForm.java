139621a3a0b4b7c0c2711f4ec27d6a0aea2c0fb7
macstudent <macstudent@10.10.150.231>
2019-07-12 11:34:07 -0400

    step 6 show the app

--- src/SimpleForm.java
+++ src/SimpleForm.java
@@ -17,7 +17,8 @@ import javafx.scene.text.Text;
 public class SimpleForm extends Application {
 
 	@Override
-	public void start(Stage primarStage) throws Exception {
+	public void start(Stage primaryStage) throws Exception {
+		
 		// * 1.Create & Configure user interface controls
 		// Label
 		Label helloLabel = new Label("Enter your name");
@@ -29,18 +30,29 @@ public class SimpleForm extends Application {
 
 		// * 2.Make a layout manager
 		VBox root = new VBox();
+		
 		// * 3.Add controls to the layout manager
 		// in same order as above
 		root.getChildren().add(helloLabel);
 		root.getChildren().add(name);
 		root.getChildren().add(goButton);
+		
 		// * 4.Add layout manager to scene
 		// * 5.Add scene to a stage
+		
+		// set height and width to 300 & 250
+		primaryStage.setScene(new Scene(root,300,250));
+		
+		//Setting the title bar
+		primaryStage.setTitle("Example 01");
+		
 		// * 6.Show the app
+		primaryStage.show();
 
 	}
 
 	public static void main(String[] args) {
+		launch(args);
 
 	}
 
