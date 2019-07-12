f941565ad604ccc2fb70662363a28606362731c9
macstudent <macstudent@10.10.150.231>
2019-07-12 11:01:48 -0400

    fxImport

--- src/FxExamle.java
+++ src/FxExamle.java
@@ -3,6 +3,17 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+
 
 public class FxExamle extends Application{
 
@@ -23,7 +34,7 @@ public class FxExamle extends Application{
 		
 	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		launch(args);
 
 	}
 
